9c617f31287941a91c665214636c4aec29750436
may <may@maydeMacBook-Pro.local>
2018-12-25 22:46:35 +0800

    Release 2.34
    1. Adapt to long package parameter
    
    Signed-off-by: yangyamei <yangyamei@xiaomi.com>

--- proxy/monkeyTest/MonkeyApkSyncUtil.py
+++ proxy/monkeyTest/MonkeyApkSyncUtil.py
@@ -2,12 +2,15 @@ from proxy.utils.ObjectSyncUtil import ObjectSyncUtil
 
 MonkeyTestApkNumber = 1
 MonkeyTestApkLocalName = "monkey_test_package.apk"
+MonkeyTestBucketName = "auto.test.apk"
 
 
 class MonkeyApkSyncUtil(ObjectSyncUtil):
-    def __init__(self, download_bucket):
-        ObjectSyncUtil.__init__(self, download_bucket,
+    def __init__(self, packages):
+        prefixes = packages + "/"
+        ObjectSyncUtil.__init__(self, MonkeyTestBucketName,
                                 MonkeyTestApkNumber,
                                 MonkeyTestApkLocalName,
+                                object_prefixes=prefixes,
                                 object_specific_name=MonkeyTestApkLocalName)
         pass
--- proxy/monkeyTest/MonkeyApkTester.py
+++ proxy/monkeyTest/MonkeyApkTester.py
@@ -67,7 +67,7 @@ class MonkeyApkTester:
         self.clear_log_folder()
         self._device_name = PropUtil.get_device_name(serial)
         self._rom_version = PropUtil.get_rom_version(serial)
-        self._MonkeyApkSyncUtil = MonkeyApkSyncUtil(self._rom_info[param.PACKAGE_NAME].lower().replace(",", "-"))
+        self._MonkeyApkSyncUtil = MonkeyApkSyncUtil(self._rom_info[param.PACKAGE_NAME])
         pass
 
     def download_test_apk(self):
