5811ec6f601f4c8f60bde8fa4b3186521a2420e6
yangyamei <yangyamei@xiaomi.com>
2019-05-04 03:23:56 +0800

    [Add][V4.2.0.1]Move presetting from global_ci_util to monkey test case
    1.Move presetting from global_ci_util to monkey test case
    
    Signed-off-by: yangyamei <yangyamei@xiaomi.com>

---
+++ proxy/preSetting/local_resources_sync_util.py
@@ -0,0 +1,20 @@
+from global_ci_util.fds.object_sync_util import ObjectSyncUtil
+
+LOCAL_RESOURCES_DOWNLOAD_BUCKET = "test.resources"
+LOCAL_RESOURCES_LOCAL_NAME = "NONE"
+LOCAL_RESOURCES_NUMBER = 60
+
+
+class LocalResourcesSyncUtil(ObjectSyncUtil):
+    def __init__(self, access_key, access_secret):
+        ObjectSyncUtil.__init__(self, access_key, access_secret, LOCAL_RESOURCES_DOWNLOAD_BUCKET,
+                                LOCAL_RESOURCES_NUMBER,
+                                LOCAL_RESOURCES_LOCAL_NAME)
+        pass
+
+    def download_objects_in_bucket_root(self, file_download_path):
+        if not file_download_path.endswith("/"):
+            file_download_path += "/"
+        self.download_objects_in_dir_in_bucket(LOCAL_RESOURCES_DOWNLOAD_BUCKET, LOCAL_RESOURCES_NUMBER, dir='',
+                                               file_download_path=file_download_path, download_file_name=None,
+                                               object_name_keyword="")
