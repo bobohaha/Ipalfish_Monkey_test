e4ee4c1784a54745175bcea38a21ea6b2c0a9ce7
yangyamei <yangyamei@xiaomi.com>
2019-05-23 10:59:40 +0800

    [Add][V4.2.0.9]Change to mongo normal db
    1. Change to mongo normal db
    
    Signed-off-by: yangyamei <yangyamei@xiaomi.com>

--- proxy/version.py
+++ proxy/version.py
@@ -1,4 +1,4 @@
-__version__ = "V4.2.0.8"
-__build__ = 0x04020008
+__version__ = "V4.2.0.9"
+__build__ = 0x04020009
 __author__ = 'yangyamei,huanglanqin'
 __author_email__ = 'yangyamei@xiaomi.com,huanglanqin@xiaomi.com'
