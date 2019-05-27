e7e994733344c9d30f7cb9e4b579cd569f859bce
yangyamei <yangyamei@xiaomi.com>
2019-05-27 11:00:55 +0800

    [Add][V4.2.0.11]Update global_ci_util_manager
    1. Update global_ci_util_manager
    
    Signed-off-by: yangyamei <yangyamei@xiaomi.com>

--- global_ci_util_manager.py
+++ global_ci_util_manager.py
@@ -1,21 +1,23 @@
 # coding=utf-8
 import os
+import time
 
 GIT_URL = "http://v9.git.n.xiaomi.com/global_common/global_ci_util"
 PROJECT_NAME = "global-ci-util"
 
 
 def check_update():
-    local_version = get_current_version()
-    print('global_ci_util_manager.check_update(): current versio %s' % local_version)
-    if local_version is not None:
+    local_version_code = version_code_local()
+    git_version_code = version_code_on_git()
+    print('global_ci_util_manager.check_update(): current version %s, git version %s' % (
+        hex(local_version_code), hex(git_version_code)))
+    if git_version_code <= local_version_code:
         # Only upgrade when new version available
-        install_command = 'pip2 install --upgrade git+%s#egg=%s' % (
-            GIT_URL, PROJECT_NAME)
-    else:
-        install_command = 'pip2 install git+%s#egg=%s' % (GIT_URL, PROJECT_NAME)
+        print('global_ci_util_manager.check_update(): not need to update global_ci_util')
+        return
 
-    print('global_ci_util_manager.check_update(): '+install_command)
+    install_command = 'pip2 install --upgrade git+%s#egg=%s' % (GIT_URL, PROJECT_NAME)
+    print('global_ci_util_manager.check_update(): ' + install_command)
     from subprocess import Popen, PIPE
     p = Popen(install_command, shell=True, stdout=PIPE,
               stderr=PIPE)
@@ -28,20 +30,30 @@ def check_update():
     # If fail to install and python suggest to add '--user', try it
     if 'Consider using the `--user`' in str(error):
         install_command = install_command + ' --user'
-        print('Try with --user: '+install_command)
+        print('Try with --user: ' + install_command)
         os.system(install_command)
 
     # If fail to use --user, try remove it
     if 'Can not perform a \'--user\' install' in str(error):
         install_command = install_command.replace(' --user', '')
-        print('Try without --user: '+install_command)
+        print('Try without --user: ' + install_command)
         os.system(install_command)
 
+    # Sleep for a while to make sure everything ready
+    time.sleep(5)
 
-def get_current_version():
+
+def version_code_local():
     try:
         import global_ci_util
-        return global_ci_util.__version__
+        return global_ci_util.__build__
     except Exception, why:
         print("get_current_version error", why)
-        return None
+        return 0
+
+
+def version_code_on_git():
+    import urllib
+    urllib.urlretrieve("%s/raw/master/global_ci_util/version.py" % GIT_URL, "global_ci_version.py")
+    import global_ci_version
+    return global_ci_version.__build__
