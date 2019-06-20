85e85d383d5577eda6cd46f41af6b8a315a1b2a1
yangyamei <yangyamei@xiaomi.com>
2019-06-20 20:52:43 +0800

    [Update][V4.2.0.19]Update issue saving info and update reopen logic
    1. Update issue saving info and update reopen logic
    
    Signed-off-by: yangyamei <yangyamei@xiaomi.com>

--- proxy/version.py
+++ proxy/version.py
@@ -1,4 +1,4 @@
-__version__ = "V4.2.0.19"
-__build__ = 0x04020018
+__version__ = "V4.2.0.20"
+__build__ = 0x04020020
 __author__ = 'yangyamei,huanglanqin'
 __author_email__ = 'yangyamei@xiaomi.com,huanglanqin@xiaomi.com'
